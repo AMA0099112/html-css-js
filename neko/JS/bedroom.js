1b7130632c7c2858a4adc6d23158fec08e258e71
AMA0099112 <99637348+AMA0099112@users.noreply.github.com>
2023-10-31 23:13:03 +0800

    Delete neko/JS directory

--- neko/JS/bedroom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#note-b").click(function () {
-        $("#page").toggle();
-    });
-    $("#batsu").click(function () {
-        $("#page").toggle();
-    })
-});
